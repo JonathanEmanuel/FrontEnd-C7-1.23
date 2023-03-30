0d0cb8d365f76cb95f1806f588ca09516be96053
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-03-30 20:16:22 -0300

    Clase 20 - getMe y cerrar sesión

--- Semana_07/ToDo/scripts/login.js
+++ Semana_07/ToDo/scripts/login.js
@@ -1,16 +1,31 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
    
-    
+    const form = document.querySelector("form");
+    const inputEmail = document.getElementById("inputEmail");
+    const inputPassword = document.getElementById("inputPassword");
 
-
+  
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
        
-        
+        event.preventDefault();
+
+        const settings = {
+            email: inputEmail.value,
+            password: inputPassword.value
+        }
+        const config = {
+            method: 'POST',
+            body: JSON.stringify(settings),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        }
+        realizarLogin(config);
 
 
 
@@ -23,11 +38,26 @@ window.addEventListener('load', function () {
     function realizarLogin(settings) {
        
 
+        const endPoint = 'https://todo-api.ctd.academy/v1/users/login';
+        fetch(endPoint, settings)
+        .then( respuesta => respuesta.json())
+        .then( (json)=> {
+            console.log(json);
+            // Guardo el jwt si el login es exitoso
+            if( json.jwt){
+                console.log('Datos Correctos');
+                localStorage.setItem('jwt', JSON.stringify(json.jwt));
+                location.replace("mis-tareas.html");
 
-
-
-        
+            } else {
+                alert('Error ' + json);
+            }
+        })
+        .catch(  error => {
+            console.log('ocurrió un error', error);
+        })
     };
+
 
 
 });
--- Semana_07/ToDo/scripts/taks.js
+++ Semana_07/ToDo/scripts/taks.js
@@ -1,13 +1,23 @@
 // SEGURIDAD: Si no se encuentra en localStorage info del usuario
 // no lo deja acceder a la página, redirigiendo al login inmediatamente.
 
+// 🖍 Si existe un token guardado
+const jwt = JSON.parse( localStorage.getItem('jwt') );
+console.log(jwt);
 
+if( ! jwt){ // Si no éxiste el jwt vamos al login
+  location.replace('index.html');
+}
 
 /* ------ comienzan las funcionalidades una vez que carga el documento ------ */
 window.addEventListener('load', function () {
 
   /* ---------------- variables globales y llamado a funciones ---------------- */
-  
+  const btnCerrarSesion = document.querySelector('#closeApp');
+  const userName = document.querySelector('.user-info p')
+  const formCrearTarea = document.querySelector('.nueva-tarea');
+  obtenerNombreUsuario();
+  console.log(userName)
 
 
   /* -------------------------------------------------------------------------- */
@@ -15,7 +25,11 @@ window.addEventListener('load', function () {
   /* -------------------------------------------------------------------------- */
 
   btnCerrarSesion.addEventListener('click', function () {
-   
+   if( confirm('¿Desar salir de la App?')  ){
+    // Elimino los datos del storage
+    localStorage.removeItem('jwt');
+    location.replace('index.html');
+   }
 
 
 
@@ -26,7 +40,23 @@ window.addEventListener('load', function () {
   /* -------------------------------------------------------------------------- */
 
   function obtenerNombreUsuario() {
-   
+    const endPoint = 'https://todo-api.ctd.academy/v1/users/getMe';
+    const config = {
+        method: 'GET',
+        headers: {
+          authorization: jwt
+        }
+    }
+
+
+    fetch(endPoint, config)
+    .then( resp => resp.json())
+    .then( data => {
+      console.log(data);
+
+      const nombre = `${data.firstName} ${data.lastName}`;
+      userName.textContent = nombre;
+    })
 
 
 
